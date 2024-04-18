698b1d938873f4476e8249332ec04f2ee1a36a38
kaustubhthakur02 <thakurkaustubh37@gmail.com>
2024-04-18 19:53:31 +0530

    added images

---
+++ script.js
@@ -0,0 +1,5 @@
+let imgClick = document.getElementsByClassName("img1")[0]
+console.log(imgClick)
+imgClick.addEventListener("mouseover", function(){
+    imgClick.style.border = "5px solid black";
+})
