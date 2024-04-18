744a8e975d29fc2f7f1220c995c6125633ecc8fb
kaustubhthakur02 <thakurkaustubh37@gmail.com>
2024-04-18 23:44:12 +0530

    added hover effect

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let imgClick = document.getElementsByClassName("img1")[0]
-console.log(imgClick)
-imgClick.addEventListener("mouseover", function(){
-    imgClick.style.border = "5px solid black";
-})
+// let imgClick = document.getElementsByClassName("img1")[0]
+// console.log(imgClick)
+// imgClick.addEventListener("mouseover", function(){
+//     imgClick.style.border = "5px solid black";
+// })
